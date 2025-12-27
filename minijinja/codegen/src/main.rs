84ad67881c0cf8d332059f0794c5869cc9810d3e
Matthias Geier <Matthias.Geier@gmail.com>
2025-12-27 16:30:01 +0100

    add first template for testing

--- minijinja/codegen/src/main.rs
+++ minijinja/codegen/src/main.rs
@@ -1,3 +1,19 @@
+use std::{fs, path::Path};
+
+use minijinja::{Environment, render};
+
 fn main() {
-    println!("Hello, world!");
+    let env = Environment::new();
+
+    // render the template and write it into the file that main.rs includes.
+    fs::write(
+        Path::new(&std::env::var("OUT_DIR").unwrap()).join("example.rs"),
+        render!(
+            in env,
+            // TODO: use runtime dependencies?
+            include_str!("../templates/src/arc.rs"),
+            build_cwd => std::env::current_dir().unwrap()
+        ),
+    )
+    .unwrap();
 }
---
+++ minijinja/codegen/templates/src/arc.rs
@@ -0,0 +1,1 @@
+// This file has been auto-generated ... {{ build_cwd }}
