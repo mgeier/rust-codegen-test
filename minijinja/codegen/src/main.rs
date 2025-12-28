c2c17af3f0bde0f92914e9f13b19d2b607317fdc
Matthias Geier <Matthias.Geier@gmail.com>
2025-12-28 17:58:57 +0100

    separate render() function

--- minijinja/codegen/src/main.rs
+++ minijinja/codegen/src/main.rs
@@ -1,13 +1,22 @@
-use std::{collections::BTreeMap, fs, path::PathBuf};
+use std::{collections::BTreeMap, fs, path::{Path, PathBuf}};
 
 use minijinja::{Environment, Value};
 use serde::Deserialize;
 
+type Context = BTreeMap<String, Value>;
+
 // TODO: --watch: only run on template file changes
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: allow passing different configurations: arc, array, bip_arc, ...
-    // TODO: if not given, scan ../configs/ dir.
+
+    // TODO: simple CLI parsing with std::env::args()
+
+    // TODO: get configs from arguments or scan config dir
+
+    // TODO: scan all templates and either
+    // (1) add them to the watch list
+    // or
+    // (2) render them with all selected configs
 
     let codegen_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR")?);
     let parent_dir = codegen_dir.join("..");
@@ -15,17 +24,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let contents = fs::read(config_path)?;
     let contents = String::from_utf8(contents)?;
     let data: Value = toml::from_str(&contents)?;
-    let ctx: BTreeMap<String, Value> = Deserialize::deserialize(data)?;
+    let ctx: Context = Deserialize::deserialize(data)?;
+
+    // TODO: create list of contexts
+    let contexts = vec![ctx];
+
+    // TODO: differentiate between "src" and "test" (an maybe others?)
+
+    let template_dir = codegen_dir.join("templates");
+    // TODO: only pass parent_dir, use that to find template dir? or use static template dir?
+    render(&template_dir, "src", "mod.rs", &contexts, &parent_dir)
+}
+
+fn render(template_dir: &Path, subdir: &str, template: &str, contexts: &[Context], parent_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let mut env = Environment::new();
-
-    // TODO: scan templates directory
-
-    let source = fs::read_to_string(codegen_dir.join("templates/src/mod.rs"))?;
-    let name = "mod.rs";
-    env.add_template(name, &source)?;
+    let source = fs::read_to_string(template_dir.join(subdir).join(template))?;
+    env.add_template(template, &source)?;
     // TODO: use env.set_loader(path_loader("templates/src"))?
-    let tmpl = env.get_template(name).unwrap();
-    let rendered = tmpl.render(ctx)?;
-    fs::write(parent_dir.join("src/arc/mod.rs"), rendered)?;
+    let tmpl = env.get_template(template).unwrap();
+    // TODO: for ctx in contexts ...
+    for ctx in contexts {
+        let rendered = tmpl.render(ctx)?;
+        // TODO: get name of context (e.g. "arc")
+        fs::write(parent_dir.join(subdir).join("arc").join(template), rendered)?;
+    }
     Ok(())
 }
